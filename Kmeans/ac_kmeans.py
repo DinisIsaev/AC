b0d377d30936d987449b60ff88616defa6eb27b5
Dinis Isaev <dinisisaev@hotmail.com>
2024-09-30 23:14:14 +0100

    Initial commit

---
+++ Kmeans/ac_kmeans.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Chooses random data points for initial centroids
+def initialize_centroids(K, X):
+    m, n = np.shape(X)
+    centroids = np.empty((K,n))
+    for i in range(K):
+        centroids[i] = X[np.random.choice(range(m))]
+    return centroids
+
+#Access file and extract data
+file = pd.read_csv('P2_CLUSTER1.csv')
+X = file.values
+N, M = np.shape(X)
+
+#Data is all clean no need to clean data
+
+#Visualize data before choosing K
+plt.subplot(2,1,1)
+plt.plot(X[:,0], X[:,1], "bo")
+plt.show()
+
+k = 1 #Choose K
+
+#Code
+initialize_centroids(k, X)
