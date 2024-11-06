6897f17712ff68f4f3cee49e8ae4c46b6b951287
Dinis Isaev <dinisisaev@gmail.com>
2024-11-06 00:02:55 +0000

    KMeans done

--- clustering/ac_kmeans.py
+++ clustering/ac_kmeans.py
@@ -4,60 +4,69 @@ import matplotlib.pyplot as plt
 
 #Chooses random data points for initial centroids
 def initialize_centroids(K, X):
-    m, n = np.shape(X)
-    centroids = np.empty((K,n))
+    l, c = np.shape(X)
+    centroids = np.empty((K,c))
     for i in range(K):
-        centroids[i] = X[np.random.choice(range(m))]
+        centroids[i] = X[np.random.choice(range(l))]
     return centroids
 
 #Euclidean distance between two vectors
 def euclidean_distance(v1,v2):
     return np.sqrt(np.sum(np.power(v1 - v2, 2)))
 
-#Find which points are closest to what centroid
+#Calculate distance of a point to each centroid
 def closest_centroid(K, centroids, x):
     distances = np.empty(K)
     for i in range(K):
         distances[i] = euclidean_distance(centroids[i], x)
     return np.argmin(distances)
 
+#Create clusters using 'closest_centroid' function
 def create_clusters(K, centroids, X):
-    N, _ = np.shape(X)
-    cluster_indexes = np.empty(N)
-    for i in range(N):
-        cluster_indexes[i] = closest_centroid(K, centroids, X[i])
-    return cluster_indexes
+    l, _ = np.shape(X)
+    clusters = np.empty(l)
+    for i in range(l):
+        clusters[i] = closest_centroid(K, centroids, X[i])
+    return clusters
 
-def cluster_centroid_means(K, cluster_indexes, X):
-    _, M = np.shape(X)
-    centroids = np.empty((K,M))
+#Calculate new centroids locations
+def cluster_centroid_means(K, clusters, X):
+    _, l = np.shape(X)
+    centroids = np.empty((K,l))
     for i in range(K):
-        points = X[cluster_indexes == i]
+        points = X[clusters == i]
         centroids[i] = np.mean(points, axis=0)
     return centroids
 
 
 #Access file and extract data
-file = pd.read_csv('P2_CLUSTER2.csv')
+file = pd.read_csv('P2_CLUSTER4.csv')
 X = file.values
-N, M = np.shape(X)
+c, l = np.shape(X)
 
-#Data is all clean no need to clean data
+#Data is all clean, no need to clean data
 
 #Visualize data before choosing K
-plt.subplot(2,1,1)
+plt.subplot(1,1,1)
 plt.plot(X[:,0], X[:,1], "bo")
 plt.show()
 
-K = 2 #Choose K
+#Choose K
+K = 2
 
-#Code
+#Run code
 centroids = initialize_centroids(K, X)
 while(True):
-    cluster_indexes = create_clusters(K, centroids, X)
+    clusters = create_clusters(K, centroids, X)
     previous_centroids = centroids
-    centroids = cluster_centroid_means(K, cluster_indexes, X)
+    centroids = cluster_centroid_means(K, clusters, X)
     diff = previous_centroids - centroids
     if not diff.any():
         break
 
+#Show clusters
+for i in range(K):
+    points = X[clusters == i]
+    plt.scatter(points[:,0], points[:,1])
+plt.plot(centroids[:][0],centroids[:][1],'r+')
+plt.show()
